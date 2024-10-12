bb52840242bce0ba69e79bca16dc4cd8f735f48c
mhmdfk <mfaaaak@hotmail.com>
2024-10-13 00:03:30 +0300

    modified some files

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
+env.config();
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "world",
-  password: "152004",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
-
 db.connect();
 
 let quiz = [
